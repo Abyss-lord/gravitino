3de9c2297251c6b4601dc724b6198f4fde96cac7
yangyang zhong <35210666+hdygxsj@users.noreply.github.com>
2025-06-18 16:57:14 +0800

    improve

--- core/src/main/java/org/apache/gravitino/storage/relational/service/OwnerMetaService.java
+++ core/src/main/java/org/apache/gravitino/storage/relational/service/OwnerMetaService.java
@@ -21,7 +21,6 @@ package org.apache.gravitino.storage.relational.service;
 import static org.apache.gravitino.metrics.source.MetricsSource.GRAVITINO_RELATIONAL_STORE_METRIC_NAME;
 
 import java.util.Collections;
-import java.util.List;
 import java.util.Optional;
 import org.apache.gravitino.Entity;
 import org.apache.gravitino.MetadataObject;
@@ -110,11 +109,6 @@ public class OwnerMetaService {
         () ->
             SessionUtils.doWithoutCommit(
                 OwnerMetaMapper.class, mapper -> mapper.insertOwnerRel(ownerRelPO)));
-  }
-
-  public List<OwnerRelPO> listOwnerByUserId(Long userId) {
-    return SessionUtils.getWithoutCommit(
-        OwnerMetaMapper.class, mapper -> mapper.listOwnerRelByUserId(userId));
   }
 
   private static long getEntityId(
